3a5c5ac7a0a8091aa2298386dac76786368b324b
daniel-agudelo9 <70704227+daniel-agudelo9@users.noreply.github.com>
2025-10-03 17:50:37 -0500

    Update Data.scala

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -4,8 +4,12 @@ object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
     val username = "john"
     val password = "demo"
+    val fromAccountId = "12456"
+    val toAccountId = "12678"
+    val amount = "1"
     
 }
+
 
 
 
