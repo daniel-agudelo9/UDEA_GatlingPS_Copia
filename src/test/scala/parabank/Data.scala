54681d64183fbf1660d0cbf6aaa870ca3c035e50
daniel-agudelo9 <70704227+daniel-agudelo9@users.noreply.github.com>
2025-10-03 23:51:52 -0500

    Update Data.scala

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -6,15 +6,14 @@ object Data{
     val password = "demo"
     val fromAccountId = "12456"
     val toAccountId = "12678"
-    val amount = "1"
 
       // IDs de ejemplo (ajusta si tu colección usa otros)
     val customerId = "12212"
-    val fromAccountId = "12456"
     val amount = "5000"
     val downPayment = "500"
     
 }
+
 
 
 
